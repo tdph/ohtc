a0be89d07e8a46b52c8acf2ceecdb05ed21d0afc
Team Developers PH <teamdevelopersph@gmail.com>
2016-08-23 03:07:59 +0800

    fonts and home finalize

--- index.php
+++ index.php
@@ -2,16 +2,37 @@
 <section class="carousel">
 
 </section>
-<div class="news-wrapper news-1">
-    <div class="c-sm-7 details">Details 1</div>
+<div class="home-content-wrapper aboutus">
+    <div class="c-sm-7 details dark">
+        <h3>ABOUT US</h3>
+        <h4>OHTC is one of the most advanced state of the art culinary and hospitality training center in the Philippines.</h4>
+        <a href="aboutus.php">READ MORE</a>
+    </div>
     <div class="c-sm-5 image"></div>
 </div>
-<div class="news-wrapper news-2">
-    <div class="c-sm-7 details">Details 2</div>
+<div class="home-content-wrapper services">
+    <div class="c-sm-7 details dark">
+        <h3>SERVICES</h3>
+        <h4>We offer presently 48 different training programs. Examples include:</h4>
+        <ul>
+            <li>Culinary Skills Development for Chief Cooks</li>
+            <li>Gallery Administration</li>
+            <li>Bread and Pastry Production</li>
+            <li>Food Safety &amp; Hygiene</li>
+            <li>Healthy Cooking at Sea and Nutritional Education</li>
+            <li>Messman to Chief Cook career Development Program</li>
+        </ul>
+        <a href="services.php">READ MORE</a>
+    </div>
     <div class="c-sm-5 image"></div>
 </div>
-<div class="news-wrapper news-3">
-    <div class="c-sm-7 details">Details 3</div>
+<div class="home-content-wrapper news">
+    <div class="c-sm-7 details">
+        <h3>IN THE NEWS</h3>
+        <h4>The Oceanic Hospitality Training Centre Launches Own Website</h4>
+        <p>One of the top hospitality and culinary training center, The Oceanic Hospitality Training Centre launches their website, www.oceanichtc.com to increase their presence in the worldwide web</p>
+        <a href="news.php">READ MORE</a>
+    </div>
     <div class="c-sm-5 image"></div>
 </div>
 <div class="video-wrapper">
