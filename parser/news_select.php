266700111f95f7977378dcf9d45c00bfe8d53e39
argiegaviola17 <ghie.gaviola17@gmail.com>
2016-08-31 10:40:50 +0800

    Update news_select.php

--- parser/news_select.php
+++ parser/news_select.php
@@ -1,16 +1,23 @@
 <?php
-//default pagination 0-10 if limit x and y
-require_once("./api.php");
-$append = (isset($_POST['id']) ? " WHERE `id`=".$_POST['id'].";" : ";");
-$qry = "SELECT `id`,`title`,`content`,`dateadded` FROM tblnews".$append;
 
-$con = new ConnectionDB();
-$api = new Api($con);
-$result = $api->ExecuteReader($qry);
+	require_once("./api.php");
 
-$arr = array();
-while($row = mysqli_fetch_row($result)){
-	$arr[] = $row;
-}
-echo json_encode($arr);
+	$append = ';'; //(isset($_POST['id']) ? " WHERE `id`=".$_POST['id'].";" : ";");
+
+	if(isset($_POST['page']) && isset($_POST['limit'])){
+
+		$arr = array('page' => $arr['page'],'limit'=>$arr['limit']);
+	  $var = new Limit($arr);
+		$append = $var->Append();
+
+	}
+	$qry = "SELECT `id`,`title`,`content`,`dateadded` FROM tblnews".$append;
+	$con = new ConnectionDB();
+	$api = new Api($con);
+	$result = $api->ExecuteReader($qry);
+	$arr = array();
+	while($row = mysqli_fetch_row($result)){
+		$arr[] = $row;
+	}
+	echo json_encode($arr);
 ?>
