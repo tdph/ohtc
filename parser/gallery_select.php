70d0d29cde1ecc87f8de71fd459aa8a883db223c
argiegaviola17 <ghie.gaviola17@gmail.com>
2016-08-31 10:40:26 +0800

    Update gallery_select.php
    new changes add a page and limit variable and change to the append variable

--- parser/gallery_select.php
+++ parser/gallery_select.php
@@ -1,16 +1,22 @@
 <?php
 
-require_once("./api.php");
-$append = (isset($_POST['id']) ? " WHERE `id`=".$_POST['id'].";" : ";");
-$qry = "SELECT `id`,`title`,`description`,`imagepath`,`dateadded` FROM tblgallery".$append;
+	require_once("./api.php");
+	$append = ';'; //(isset($_POST['id']) ? " WHERE `id`=".$_POST['id'].";" : ";");
 
-$con = new ConnectionDB();
-$api = new Api($con);
-$result = $api->ExecuteReader($qry);
+	if(isset($_POST['page']) && isset($_POST['limit'])){
 
-$arr = array();
-while($row = mysqli_fetch_row($result)){
-	$arr[] = $row;
-}
-echo json_encode($arr);
+		$arr = array('page' => $arr['page'],'limit'=>$arr['limit']);
+		$var = new Limit($arr);
+		$append = $var->Append();
+
+	}
+	$qry = "SELECT `id`,`title`,`description`,`imagepath`,`dateadded` FROM tblgallery".$append;
+	$con = new ConnectionDB();
+	$api = new Api($con);
+	$result = $api->ExecuteReader($qry);
+	$arr = array();
+	while($row = mysqli_fetch_row($result)){
+		$arr[] = $row;
+	}
+	echo json_encode($arr);
 ?>
