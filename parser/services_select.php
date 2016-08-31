a71defb3a39c4ef44f828a11a0e3602f3bcf9e39
argiegaviola17 <ghie.gaviola17@gmail.com>
2016-08-31 10:41:29 +0800

    Update services_select.php

--- parser/services_select.php
+++ parser/services_select.php
@@ -2,7 +2,16 @@
 
     require_once("./api.php");
 
-    $append = (isset($_POST['id']) ? " WHERE `id`=".$_POST['id'].";" : ";");
+    $append = ';'; //(isset($_POST['id']) ? " WHERE `id`=".$_POST['id'].";" : ";");
+
+    if(isset($_POST['page']) && isset($_POST['limit'])){
+
+    	$arr = array('page' => $arr['page'],'limit'=>$arr['limit']);
+      $var = new Limit($arr);
+    	$append = $var->Append();
+
+    }
+
     $qry = "SELECT `id`,`name`,`description`,`minstudents`,`maxstudents`,`duration`,`imagepath`,`dateadded` FROM `tblservices`".$append;
 
     $con = new ConnectionDB();
