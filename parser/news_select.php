5f24ca642c81f3c6d352f0b8cede186df4cd6d59
argiegaviola17 <ghie.gaviola17@gmail.com>
2016-09-09 14:27:36 +0800

    News select.php updated

--- parser/news_select.php
+++ parser/news_select.php
@@ -1,23 +1,23 @@
 <?php
 
-	require_once("./api.php");
+	require_once("../api.php");
 
 	$append = ';'; //(isset($_POST['id']) ? " WHERE `id`=".$_POST['id'].";" : ";");
 
 	if(isset($_POST['page']) && isset($_POST['limit'])){
 
-		$arr = array('page' => $arr['page'],'limit'=>$arr['limit']);
+		$arr = array('page' => $_POST['page'],'limit'=>$_POST['limit']);
 	  $var = new Limit($arr);
 		$append = $var->Append();
 
 	}
-	$qry = "SELECT `id`,`title`,`content`,`dateadded` FROM tblnews".$append;
+	$qry = "SELECT `id`,`title`,`content`,`dateadded`,`imagepath` FROM tblnews".$append;
 	$con = new ConnectionDB();
 	$api = new Api($con);
 	$result = $api->ExecuteReader($qry);
 	$arr = array();
 	while($row = mysqli_fetch_row($result)){
-		$arr[] = $row;
+		$arr[] = array("id"=>$row[0],"title"=>$row[1],"content"=>$row[2],"dateadded"=>$row[3],"imagepath"=>$row[4]);
 	}
 	echo json_encode($arr);
 ?>
